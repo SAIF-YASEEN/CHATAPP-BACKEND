450cd53a6d1f3eb2e4b72fa3da89a4302c51cdc9
SAIF-YASEEN <146560201+SAIF-YASEEN@users.noreply.github.com>
2025-02-18 14:49:39 -0700

    url aded new ones

--- src/index.js
+++ src/index.js
@@ -16,33 +16,19 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
+app.use(
+  cors({
+    origin: [
+      "https://test-repo-production-5712.up.railway.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  "https://test-repo-production-5712.up.railway.app/",
-  "http://localhost:5173", // Development
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Allows cookies & tokens
-  })
-);
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Frontend URL
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
-  })
-);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
